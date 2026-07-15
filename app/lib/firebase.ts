09cb752feda44e5439f003306ecb346bf22c5f3b
Sreyasree Satapathy <sreyasreesatapathy@gmail.com>
2026-07-15 16:25:50 +0530

    Fix duplicate Firebase initialization

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,6 +1,8 @@
-import { initializeApp } from "firebase/app";
+// Import Firebase
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCuqVvGGX_2-y2g1RVYwhv1HRXbnx2MH_Q",
   authDomain: "instagram-reels-sreyasre-18e69.firebaseapp.com",
@@ -8,8 +10,14 @@ const firebaseConfig = {
   storageBucket: "instagram-reels-sreyasre-18e69.firebasestorage.app",
   messagingSenderId: "504595732985",
   appId: "1:504595732985:web:2f7a8d10ad2484990cc528",
+  measurementId: "G-WH99FB9225",
 };
 
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase only once
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
+// Firestore
 export const db = getFirestore(app);
+
+// Export app
+export { app };
